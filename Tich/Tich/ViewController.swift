b446a0a207fa28fdbe8c2b1879975679509af939
LimCharlie <lim_c@etna-alternance.net>
2016-07-21 04:34:49 -0700

    Material + front page Login + Inscription
    pod install

--- Tich/Tich/ViewController.swift
+++ Tich/Tich/ViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import Material
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Identifiant: TextField!
+    
+   
+    @IBOutlet weak var MotDePasse: TextField!
+    
+    @IBOutlet weak var BttConnexion: FlatButton!
+    
+    @IBOutlet weak var BttInscription: RaisedButton!
     @IBAction func Connexion(sender: UIButton) {
         performSegueWithIdentifier("nextview", sender: self)
     }
@@ -21,8 +30,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        prepareStyle()
     }
   
+    
+    
+    private func prepareStyle() {
+        // style input connexion
+        Identifiant.placeholder = "Identifiant"
+        Identifiant.placeholderColor = MaterialColor.white
+        Identifiant.placeholderActiveColor = MaterialColor.green.base
+        
+        //style du button connexion
+        BttConnexion.setTitle("Connexion", forState: .Normal)
+        BttConnexion.setTitleColor(MaterialColor.blue.base, forState: .Normal)
+        BttConnexion.pulseColor = MaterialColor.blue.base
+        
+        //style button inscription
+        BttInscription.setTitle("Inscription", forState: .Normal)
+        BttInscription.setTitleColor(MaterialColor.blue.base, forState: .Normal)
+        BttInscription.pulseColor = MaterialColor.blue.base
+        
+        //style input mdp
+        MotDePasse.placeholder = "Mot de passe"
+        MotDePasse.placeholderColor = MaterialColor.white
+        MotDePasse.placeholderActiveColor = MaterialColor.green.base
+       
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- Tich/Tich/inscription.swift
+++ Tich/Tich/inscription.swift
@@ -8,6 +8,7 @@
 
 
 import UIKit
+import Material
 
 class InscriptionController: UIViewController {
  
@@ -15,17 +16,56 @@ class InscriptionController: UIViewController {
           performSegueWithIdentifier("InscOkay", sender: self)
     }
     
+    @IBOutlet weak var InputEmail: TextField!
+    
+    @IBOutlet weak var InputMdp1: TextField!
+    @IBOutlet weak var Mdp2: TextField!
+    
+    @IBOutlet weak var Retour: RaisedButton!
+    @IBOutlet weak var bttInscrip: RaisedButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+          prepareStyle()
     }
     
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+      
     }
     
     
-}
+    
+    private func prepareStyle() {
+        // style input email
+        InputEmail.placeholder = "Email"
+        InputEmail.placeholderColor = MaterialColor.white
+        InputEmail.placeholderActiveColor = MaterialColor.green.base
+        
+       // style input mdp
+        InputMdp1.placeholder = "Mot de Passe"
+        InputMdp1.placeholderColor = MaterialColor.white
+        InputMdp1.placeholderActiveColor = MaterialColor.green.base
+        
+        // style input2
+        
+        Mdp2.placeholder = "Confirme votre mot de passe"
+        Mdp2.placeholderColor = MaterialColor.white
+        Mdp2.placeholderActiveColor = MaterialColor.green.base
+        
+        // style btt inscription
+        
+        bttInscrip.setTitle("Valider", forState: .Normal)
+        bttInscrip.setTitleColor(MaterialColor.blue.base, forState: .Normal)
+        bttInscrip.pulseColor = MaterialColor.blue.base
+        
 
+        let img: UIImage? = MaterialIcon.cm.arrowBack
+        Retour.setImage(img, forState: .Normal)
+        Retour.setImage(img, forState: .Highlighted)
+    
+    }
+}
