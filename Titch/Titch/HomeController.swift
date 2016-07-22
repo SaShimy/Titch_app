8dba797789aa41bf0b276abbd46766f7e6b00a1d
SaShimy <antoinegunzburger@hotmail.fr>
2016-07-22 11:25:19 +0200

    blbl

--- Titch/Titch/HomeController.swift
+++ Titch/Titch/HomeController.swift
@@ -16,6 +16,9 @@ class HomeController: UIViewController, UIPopoverPresentationControllerDelegate 
     @IBOutlet weak var genderText: UILabel!
     @IBOutlet weak var firstnameText: UILabel!
     @IBOutlet weak var lastnameText: UILabel!
+    @IBAction func refresh(sender: AnyObject) {
+        loadUser()
+    }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "popUp" {
             let popoverViewController = segue.destinationViewController
@@ -28,29 +31,34 @@ class HomeController: UIViewController, UIPopoverPresentationControllerDelegate 
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(token)
-        let headers = [
-            "Authorization": "Bearer " + token
-        ]
-        Alamofire.request(.GET, "http://symfonyios.cloudapp.net/api/user/me", headers: headers)
-        .validate()
-        .responseJSON { response in
-            if (response.result.value != nil) {
-                debugPrint(response.result.value)
-                let json = JSON(response.result.value!)
-                self.emailText.text = "Email : " + json["email"].stringValue
-                self.ageText.text = "Age : " + json["age"].stringValue
-                self.genderText.text = "Sexe : " + json["gender"].stringValue
-                self.firstnameText.text = "Prénom : " + json["firstname"].stringValue
-                self.lastnameText.text = "Sexe : " + json["lastname"].stringValue
-            }
-        }
-
+        loadUser()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func popoverPresentationControllerDidDismissPopover(popoverPresentationController: UIPopoverPresentationController) {
+        loadUser()
+    }
+    
+    func loadUser() {
+        let headers = [
+            "Authorization": "Bearer " + token
+        ]
+        Alamofire.request(.GET, "http://symfonyios.cloudapp.net/api/user/me", headers: headers)
+            .validate()
+            .responseJSON { response in
+                if (response.result.value != nil) {
+                    debugPrint(response.result.value)
+                    let json = JSON(response.result.value!)
+                    self.emailText.text = "Email : " + json["email"].stringValue
+                    self.ageText.text = "Age : " + json["age"].stringValue
+                    self.genderText.text = "Sexe : " + json["gender"].stringValue
+                    self.firstnameText.text = "Prénom : " + json["firstname"].stringValue
+                    self.lastnameText.text = "Sexe : " + json["lastname"].stringValue
+                }
+        }
     }
 }
--- Titch/Titch/PopUpController.swift
+++ Titch/Titch/PopUpController.swift
@@ -7,7 +7,31 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class PopUpController: UIViewController {
 
+    @IBOutlet weak var fName: UITextField!
+    @IBOutlet weak var lName: UITextField!
+    @IBOutlet weak var gender: UISegmentedControl!
+    @IBOutlet weak var age: UITextField!
+    @IBAction func sendProfile(sender: AnyObject) {
+        let genderV: String = gender.titleForSegmentAtIndex(gender.selectedSegmentIndex)!
+        let ageV = Int(age.text!)
+        let headers = [
+            "Authorization": "Bearer " + token
+        ]
+        let parameters : [String : AnyObject] = [
+            "firstname": fName.text!,
+            "lastname": lName.text!,
+            "age": ageV!,
+            "gender": genderV
+        ]
+        print(headers)
+        print(parameters)
+        Alamofire.request(.PUT, "http://symfonyios.cloudapp.net/api/user/update", headers: headers,parameters: parameters)
+        .validate()
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
