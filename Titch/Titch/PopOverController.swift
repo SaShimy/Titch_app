620b3193334f4254abd3683f19185773c4b9edbe
SaShimy <antoinegunzburger@hotmail.fr>
2016-07-29 11:27:11 +0200

    jh

--- Titch/Titch/PopOverController.swift
+++ Titch/Titch/PopOverController.swift
@@ -68,6 +68,6 @@ class PopOverController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }		
 
 }
